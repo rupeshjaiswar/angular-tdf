edd98c3c08f57ffa9f61e0abfbe3a75a0df4d600
Rupesh <jaiswar.rupesh31@gmail.com>
2021-06-01 15:29:28 +0530

    angular template driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './user';
+import { EnrollmentService } from './enrollment.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tdf';
+
+  //array of topics to select
+  topics = ["Angular", "React", "Vue"];
+  topicHasError = true;
+  submitted = false;
+  errorMsg = '';
+
+  userModel = new User('Rupesh', 'rupesh@test.com', 6383273124, 'default', 'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService){ }
+
+  validateTopic(value:any){
+    if (value === 'default') {
+      this.topicHasError = true;
+    } else {
+      this.topicHasError = false;
+    }
+  }
+
+  onSubmit(userForm : any) {
+
+    console.log(userForm);
+    // this.submitted = true;
+    // this._enrollmentService.enroll(this.userModel)
+    // .subscribe(
+    //   data => console.log('Success', data),
+    //   // error => console.error('Error!', error)
+    //   //assigning error status text
+    //   error => this.errorMsg = error.statusText
+    // );
+  }
+
+
 }
